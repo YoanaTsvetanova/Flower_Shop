6486e5f40bf93dd00138e06d30360e8b26c8b502
Yoana <yoana.tsvetanova06@gmail.com>
2025-08-13 16:07:25 +0300

    Creates a navbar with a hamburger menu and a css style sheet both of which i included in the index.php. Also created a logout page.

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php 
 session_start();
 include 'config.php'; 
-
 $is_logged_in = isset($_SESSION['id']) && $_SESSION['id'];
 ?>
-
 <!DOCTYPE html>
 <html lang="bg">
 <head>
     <meta charset="UTF-8">
     <title>Онлайн Магазин за Цветя</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+
+<?php include 'navbar.php'; ?>
+
+<main style="padding: 20px;">
+   
+</main>
+
 </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,48 @@
+<?php
+$is_logged_in = isset($_SESSION['id']) && $_SESSION['id'];
+?>
+<header>
+    <div class="header-container">
+  
+        <button class="hamburger-menu" aria-label="Menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+
+  
+        <div class="nav-menu">
+        
+            <?php /*
+            <div class="nav-menu__section">
+                <a href="index.php">
+                    <img src="images/logo.png" alt="Logo" class="logo">
+                </a>
+            </div>
+            */ ?>
+
+          
+            <div class="nav-menu__section">
+                <a class="nav-menu__link" href="index.php">Начало</a>
+                <a class="nav-menu__link" href="products.php">Продукти</a>
+                <a class="nav-menu__link" href="blog.php">Блог</a>
+                <?php if ($is_logged_in): ?>
+                    <a class="nav-menu__link" href="favourites.php">Любими</a>
+                <?php endif; ?>
+            </div>
+
+        
+            <div class="nav-menu__section">
+                <?php if ($is_logged_in): ?>
+                    <a class="nav-menu__link" href="profile.php">Профил</a>
+                    <a class="nav-menu__link" href="logout.php">Изход</a>
+                <?php else: ?>
+                    <a class="nav-menu__link" href="login.php">Вход</a>
+                    <a class="nav-menu__link" href="register.php">Регистрация</a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</header>
+
+<script src="js/header.js"></script>
